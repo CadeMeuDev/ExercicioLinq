76c4865d8322aaa474d6ba309205ee9ca1dabeee
jersonb <jersonlbrito@hotmail.com>
2024-12-19 15:32:17 -0300

    number question

--- ExercicioLinq/ExercicioLinqTests.cs
+++ ExercicioLinq/ExercicioLinqTests.cs
@@ -2,24 +2,21 @@ namespace ExercicioLinq
 {
     public class ExercicioLinqTests
     {
-        private List<Produto> produtos;
+        private readonly List<Produto> produtos =
+            [
+                new Produto { Nome = "Sabão", Valor = 1.1m, Quantidade = 10 },
+                new Produto { Nome = "Detergente de prato", Valor = 10, Quantidade = 9 },
+                new Produto { Nome = "Água", Valor = (decimal)8.2f, Quantidade = 8 },
+                new Produto { Nome = "Esponja", Valor = (decimal)5.5, Quantidade = 7 },
+                new Produto { Nome = "Água sanitária", Valor = (decimal)30.30d, Quantidade = 6 },
+                new Produto { Nome = "Vassoura", Valor = 3.3m, Quantidade = 5 },
+                new Produto { Nome = "Desinfetante", Valor = 4.4m, Quantidade = 4 },
+                new Produto { Nome = "Pano de chão", Valor = 5.5m, Quantidade = 3 },
+                new Produto { Nome = "Purificador de água", Valor = 6.6m, Quantidade = 2 },
+                new Produto { Nome = "Balde", Valor = 10.1m, Quantidade = 1 },
+            ];
 
-        public ExercicioLinqTests()
-        {
-            produtos = new List<Produto>();
-            produtos.Add(new Produto { Nome = "Sabão", Valor = 1.1m, Quantidade = 10 });
-            produtos.Add(new Produto { Nome = "Detergente de prato", Valor = 10, Quantidade = 9 });
-            produtos.Add(new Produto { Nome = "Água", Valor = (decimal)8.2f, Quantidade = 8 });
-            produtos.Add(new Produto { Nome = "Esponja", Valor = (decimal)5.5, Quantidade = 7 });
-            produtos.Add(new Produto { Nome = "Água sanitária", Valor = (decimal)30.30d, Quantidade = 6 });
-            produtos.Add(new Produto { Nome = "Vassoura", Valor = 3.3m, Quantidade = 5 });
-            produtos.Add(new Produto { Nome = "Desinfetante", Valor = 4.4m, Quantidade = 4 });
-            produtos.Add(new Produto { Nome = "Pano de chão", Valor = 5.5m, Quantidade = 3 });
-            produtos.Add(new Produto { Nome = "Purificador de água", Valor = 6.6m, Quantidade = 2 });
-            produtos.Add(new Produto { Nome = "Balde", Valor = 10.1m, Quantidade = 1 });
-        }
-
-        [Fact(DisplayName = "Quantidade de produtos que possuem a palavra 'água' no nome.")]
+        [Fact(DisplayName = "01-Quantidade de produtos que possuem a palavra 'água' no nome.")]
         public void Test1()
         {
             int quantidade = 0;
@@ -27,7 +24,7 @@ namespace ExercicioLinq
             Assert.Equal(3, quantidade);
         }
 
-        [Fact(DisplayName = "Produtos ordenados por nome.")]
+        [Fact(DisplayName = "02-Produtos ordenados por nome.")]
         public void Test2()
         {
             IEnumerable<Produto> produtosOrdenados = null;
@@ -36,7 +33,7 @@ namespace ExercicioLinq
             Assert.Equal("Vassoura", produtosOrdenados.Last().Nome);
         }
 
-        [Fact(DisplayName = "Produtos ordenados do mais caro para o mais barato.")]
+        [Fact(DisplayName = "03-Produtos ordenados do mais caro para o mais barato.")]
         public void Test3()
         {
             IEnumerable<Produto> produtosOrdenados = null;
@@ -45,7 +42,7 @@ namespace ExercicioLinq
             Assert.Equal("Sabão", produtosOrdenados.Last().Nome);
         }
 
-        [Fact(DisplayName = "Produto mais caro")]
+        [Fact(DisplayName = "04-Produto mais caro")]
         public void Test4()
         {
             Produto produto = null;
@@ -53,7 +50,7 @@ namespace ExercicioLinq
             Assert.Equal("Água sanitária", produto.Nome);
         }
 
-        [Fact(DisplayName = "Produto mais barato")]
+        [Fact(DisplayName = "05-Produto mais barato")]
         public void Test5()
         {
             Produto produto = null;
@@ -61,7 +58,7 @@ namespace ExercicioLinq
             Assert.Equal("Sabão", produto.Nome);
         }
 
-        [Fact(DisplayName = "Lista dos nomes dos produtoss")]
+        [Fact(DisplayName = "06-Lista dos nomes dos produtoss")]
         public void Test6()
         {
             IEnumerable<string> nomeDosProdutos = null;
@@ -69,7 +66,7 @@ namespace ExercicioLinq
             Assert.Contains("Água", nomeDosProdutos);
         }
 
-        [Fact(DisplayName = "Quantidade total de todos dos produtos")]
+        [Fact(DisplayName = "07-Quantidade total de todos dos produtos")]
         public void Test7()
         {
             int quantidade = 0;
@@ -77,7 +74,7 @@ namespace ExercicioLinq
             Assert.Equal(55, quantidade);
         }
 
-        [Fact(DisplayName = "Nome dos produtos com valor até 10.0")]
+        [Fact(DisplayName = "08-Nome dos produtos com valor até 10.0")]
         public void Test8()
         {
             IEnumerable<string> nomeDosProdutos = null;
@@ -86,7 +83,7 @@ namespace ExercicioLinq
             Assert.Contains("Sabão", nomeDosProdutos);
         }
 
-        [Fact(DisplayName = "Nome dos produtos com valor maior 10.0")]
+        [Fact(DisplayName = "09-Nome dos produtos com valor maior 10.0")]
         public void Test9()
         {
             IEnumerable<string> nomeDosProdutos = null;
@@ -95,7 +92,7 @@ namespace ExercicioLinq
             Assert.Contains("Água sanitária", nomeDosProdutos);
         }
 
-        [Fact(DisplayName = "Verifica se o produto 'pão' está na lista")]
+        [Fact(DisplayName = "10-Verifica se o produto 'pão' está na lista")]
         public void Test10()
         {
             bool existe = true;
